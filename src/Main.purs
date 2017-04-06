98de711ad9d50ea00afee490fc61ad8c7c61f3d1
Krzysztof Langner <klangner@gmail.com>
2017-04-06 09:13:49 +0200

    Upload data from the server

--- src/Main.purs
+++ src/Main.purs
@@ -4,10 +4,11 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
-
+import Network.HTTP.Affjax as AX
 import Page (page)
 
-main :: Eff (HA.HalogenEffects ()) Unit
+
+main :: Eff (HA.HalogenEffects (ajax :: AX.AJAX)) Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
   runUI page unit body
--- src/Page.purs
+++ src/Page.purs
@@ -1,41 +1,44 @@
 module Page where
 
 import Prelude
+import Control.Monad.Aff (Aff)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Data.Maybe (Maybe(..))
+import Network.HTTP.Affjax as AX
 
 
 data Query a = LoadSeries a
-             | SeriesLoaded a
 
-type State = { series :: Maybe String }
+type State = { loading :: Boolean
+             , series :: Maybe String 
+             }
 
-page :: forall m. H.Component HH.HTML Query Unit Void m
+page :: ∀ eff. H.Component HH.HTML Query Unit Void (Aff (ajax :: AX.AJAX | eff))
 page =
   H.component
     { initialState: const initialState  -- query -> state const will creatre function which ignore query param
-    , render                            --  state -> HTML
+    , render                            -- state -> HTML
     , eval                              -- Query ->ComponentDSL state query output monad
     , receiver: const Nothing           -- 
     }
   
 
 initialState :: State
-initialState = { series: Nothing }
+initialState = { loading: false, series: Nothing }
+
 
 render :: State -> H.ComponentHTML Query
 render state =
   HH.div_ 
     [ renderNavibar 
     , renderChart state
-    , renderUploadSection
     ]
     
 
-renderNavibar :: forall p. HH.HTML p (Query Unit)
+renderNavibar :: ∀ p. HH.HTML p (Query Unit)
 renderNavibar = 
   HH.nav
     [ HP.class_ (H.ClassName "navbar navbar-default") ]
@@ -48,13 +51,21 @@ renderNavibar =
             , HP.href "#"]
             [ HH.text "Time Series Lab" ]
           ]
+      , HH.div 
+          [ HP.class_ (H.ClassName "container") ]
+          [ HH.button
+              [ HP.class_ (H.ClassName "btn btn-primary navbar-btn navbar-left")
+              ,HE. onClick (HE.input_ LoadSeries)
+              ]
+              [ HH.text "Upload CSV" ]
+          ]
       ]
     ]
 
 
 -- Render Chart based on provided Time Series.
 -- If there is no Series then render empty space
-renderChart :: forall p. State -> HH.HTML p (Query Unit)
+renderChart :: ∀ p. State -> HH.HTML p (Query Unit)
 renderChart state = 
   HH.div 
     [ HP.class_ (H.ClassName "panel panel-default") ]
@@ -64,40 +75,11 @@ renderChart state =
     ]
 
 
--- Render upload button
-renderUploadSection :: forall p. HH.HTML p (Query Unit)
-renderUploadSection = 
-  HH.div 
-    [ HP.class_ (H.ClassName "panel panel-default") ]
-    [ HH.div 
-      [ HP.class_ (H.ClassName "panel-body") ]
-      [ HH.text "Select CSV file" 
-      , HH.div_
-        [ HH.input 
-          [ HP.type_ HP.InputFile
-          , HP.id_ "fileInput"
-          , HE.onChange (HE.input_ LoadSeries)
-          ] 
-        ]
-      ]
-    ]
-  
-
-
 -- Query evaluation      
-    -- var file = fileInput.files[0];
-    -- var reader = new FileReader();
-    -- reader.onload = function(e) {
-    --     csvUploaded(reader.result);
-    -- }
-    -- reader.readAsText(file);
-eval :: forall m. Query ~> H.ComponentDSL State Query Void m
+eval :: ∀ eff. Query ~> H.ComponentDSL State Query Void (Aff (ajax :: AX.AJAX | eff))
 eval = case _ of
-  
   LoadSeries next -> do
-    --  d <- document =<< window
-    H.modify (\state -> { series: Just "loaded" })
-    pure next
-
-  SeriesLoaded next -> do
+    H.modify (_ { loading = true })
+    response <- H.liftAff $ AX.get ("https://timeserieslab.github.io/testdata/small.csv")
+    H.modify (_ { loading = false, series = Just response.response })
     pure next
