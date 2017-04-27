1fddfd6514e8f051d03579246d2f91e37e3e113d
Krzysztof Langner <klangner@gmail.com>
2017-04-27 14:05:02 +0200

    Before Halogen back

--- src/Main.purs
+++ src/Main.purs
@@ -4,9 +4,18 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+import DOM.HTML (window)
+import DOM.HTML.Window (document)
+
+
+main :: forall e. Eff (console :: CONSOLE, dom :: DOM | e) Unit
 main = do
-  log "Time Series Lab loaded."
+  connectButtons
 
 
-
+-- | Connect buttons loading example datasets
+connectButtons :: forall e. Eff (console :: CONSOLE, dom :: DOM | e) Unit
+connectButtons = do
+  doc <- document <$> window
+  log doc
+  -- getElementById "" doc
